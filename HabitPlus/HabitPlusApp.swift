31c6703ce63a11c6232b823fdd2d26876e175b83
Ingrid Silveira <ingridgsilveira@gmail.com>
2023-07-27 18:51:07 -0300

    Initial Commit

---
+++ HabitPlus/HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusApp.swift
+//  HabitPlus
+//
+//  Created by userext on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
